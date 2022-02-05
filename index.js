d87c56492944f893b762e0e6d0fd67284de1dc21
jaberali <jaber2024@gmail.com>
2022-02-04 23:50:27 -0500

    modifying code

---
+++ index.js
@@ -0,0 +1,40 @@
+
+function fun() {
+  alert ('  " Welcome to Promach " , Location : G - R1L1 + R4L7 ' ) ;
+}
+function fun1() {
+  alert ('  " Welcome to Promach " , Location : G - R1L2' ) ;
+}
+function fun2() {
+  alert ('  " Welcome to Promach " , Location : G - R2L2 ' ) ;
+}
+function fun3() {
+  alert ('  " Welcome to Promach " , Location : G - R1L3 ' ) ;
+}
+function fun4() {
+  alert ('  " Welcome to Promach " , Location : G - R1L4 ' ) ;
+}
+function fu5() {
+  alert ('  " Welcome to Promach " , Location : G - R2L1  ' ) ;
+}
+function fun6() {
+  alert ('  " Welcome to Promach " , Location : G - R1L7 ' ) ;
+}
+function fun7() {
+  alert ('  " Welcome to Promach " , Location : G - R1L8 ' ) ;
+}
+function fun8() {
+  alert ('  " Welcome to Promach " , Location : G - R4L12 + R1L6 ' ) ;
+}
+function fun9() {
+  alert ('  " Welcome to Promach " , Location : G - R1L9 ' ) ;
+}
+function fun10() {
+  alert ('  " Welcome to Promach " , Location : G - R3L4 ' ) ;
+}
+function fun11() {
+  alert ('  " Welcome to Promach " , Location : G- R1L9 ' ) ;
+}
+function fun12() {
+  alert ('  " Welcome to Promach " , Location : G - R1L1 ' ) ;
+}
