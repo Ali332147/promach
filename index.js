f78f454cec6d6e8ae34d07725e152121358a6d04
jaberali <jaber2024@gmail.com>
2022-02-07 20:41:41 -0500

    final

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ function fun3() {
 function fun4() {
   alert ('  " Welcome to Promach " , Location : G - R1L4 ' ) ;
 }
-function fu5() {
+function fun5() {
   alert ('  " Welcome to Promach " , Location : G - R2L1  ' ) ;
 }
 function fun6() {
   alert ('  " Welcome to Promach " , Location : G - R1L7 ' ) ;
 }
 function fun7() {
-  alert ('  " Welcome to Promach " , Location : G - R1L8 ' ) ;
+  alert ('  " Welcome to Promach " , Location : C - R4L1 ... C - R4L6 ' ) ;
 }
 function fun8() {
   alert ('  " Welcome to Promach " , Location : G - R4L12 + R1L6 ' ) ;
@@ -40,8 +40,109 @@ function fun12() {
 }
 function fun13() {
   alert ('  " Welcome to Promach " , Location : Tom Places ' ) ;
-}function fun14() {
+}
+function fun14() {
   alert ('  " Welcome to Promach " , Location : Tom Places ' ) ;
-}function fun15() {
+}
+function fun15() {
   alert ('  " Welcome to Promach " , Location : Tom Places ' ) ;
+}
+// from here
+function fun16() {
+  alert ('  " Welcome to Promach " , Location : G - R1L11 ' ) ;
+}
+function fun17() {
+  alert ('  " Welcome to Promach " , Location : G - R1L1 ' ) ;
+}
+function fun18() {
+  alert ('  " Welcome to Promach " , Location : H - R1L1 R.SIDE ' ) ;
+}
+function fun19() {
+  alert ('  " Welcome to Promach " , Location : H - R1L2 ' ) ;
+}
+function fun20() {
+  alert ('  " Welcome to Promach " , Location : H - R1L3 ' ) ;
+}
+function fun21() {
+  alert ('  " Welcome to Promach " , Location : H - R1L4 ' ) ;
+}
+function fun22() {
+  alert ('  " Welcome to Promach " , Location : H - R1L5 ' ) ;
+}
+function fun23() {
+  alert ('  " Welcome to Promach " , Location : H - R2L1 ' ) ;
+}
+function fun24() {
+  alert ('  " Welcome to Promach " , Location : H - R2L10 + R3L4' ) ;
+}
+function fun25() {
+  alert ('  " Welcome to Promach " , Location : H - R2L2 ' ) ;
+}
+
+function fun26() {
+  alert ('  " Welcome to Promach " , Location : H - R2L4 + R3L1' ) ;
+}
+function fun27() {
+  alert ('  " Welcome to Promach " , Location : H - R2L5 ' ) ;
+}
+function fun28() {
+  alert ('  " Welcome to Promach " , Location : H - R2L6 ' ) ;
+}
+function fun29() {
+  alert ('  " Welcome to Promach " , Location : H - R3L3  ' ) ;
+}
+function fun30() {
+  alert ('  " Welcome to Promach " , Location : H - R4L3 --- R4L6 ' ) ;
+}
+function fun31() {
+  alert ('  " Welcome to Promach " , Location : H - R2L7 ' ) ;
+}
+function fun32() {
+  alert ('  " Welcome to Promach " , Location : H - R2L8 ' ) ;
+}
+function fun33() {
+  alert ('  " Welcome to Promach " , Location : H - R2L9 L.SIDE ' ) ;
+}
+function fun34() {
+  alert ('  " Welcome to Promach " , Location : H - R2L9 ' ) ;
+}
+function fun35() {
+  alert ('  " Welcome to Promach " , Location : H - R2L10 ' ) ;
+}
+function fun36() {
+  alert ('  " Welcome to Promach " , Location : H - R4L1 ' ) ;
+}
+function fun37() {
+  alert ('  " Welcome to Promach " , Location : H - R1L7 ' ) ;
+}
+function fun38() {
+  alert ('  " Welcome to Promach " , Location : H - R1L8 ' ) ;
+}
+function fun39() {
+  alert ('  " Welcome to Promach " , Location : H - R1L9 ' ) ;
+}
+function fun40() {
+  alert ('  " Welcome to Promach " , Location : H - R1L10 ' ) ;
+}
+function fun41() {
+  alert ('  " Welcome to Promach " , Location : H - R1L11 ' ) ;
+}
+function fun42() {
+  alert ('  " Welcome to Promach " , Location : H - R1L12 ' ) ;
+}
+function fun43() {
+  alert ('  " Welcome to Promach " , Location : H - R4L9 ' ) ;
+}
+function fun44() {
+  alert ('  " Welcome to Promach " , Location : H - R2L11 ' ) ;
+}
+
+function fun45() {
+  alert ('  " Welcome to Promach " , Location : C - R2L1 ---R2L4 ' ) ;
+}
+function fun46() {
+  alert ('  " Welcome to Promach " , Location : C - R2L7 ' ) ;
+}
+function fun47() {
+  alert ('  " Welcome to Promach " , Location : C - R2L9 ---R2L12 ' ) ;
 }
